efb8e2fcd08c01ced6fc00e23f2db0f7079a3ca9
tigaron <suputra@mail.com>
2022-10-08 06:09:10 +0800

    wip: trying to fix invalid signature

--- libs/saml.ts
+++ libs/saml.ts
@@ -5,6 +5,12 @@ samlify.setSchemaValidator(validator);
 
 export const idp = samlify.IdentityProvider({
 	metadata: Buffer.from(process.env.IDP_METADATA as string, 'base64'),
+	wantAuthnRequestsSigned: true,
+	wantLogoutRequestSigned: true,
+	wantLogoutResponseSigned: true,
+	signingCert: Buffer.from(process.env.SIGN_B64_CERTIFICATE as string, 'base64'),
+	privateKey: Buffer.from(process.env.SIGN_B64_PRIVATE_KEY as string, 'base64'),
+	privateKeyPass: process.env.SP_PRIVATE_KEY_PASS,
 });
 
 export const sp = samlify.ServiceProvider({
